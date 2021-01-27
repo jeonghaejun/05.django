800bdfba22488a908e7eec5ef64bfe928983ccc1
wjdgownsll <wjdgownsll@gmail.com>
2021-01-27 14:19:40 +0900

    21.01.27 2nd 05.django

--- web_app2/blog/urls.py
+++ web_app2/blog/urls.py
@@ -9,11 +9,17 @@ urlpatterns = [
     # Example: /blog/archive/
     path('archive/', PostAV.as_view(), name='post_archive'),
     # Example: /blog/archive/2019/
-    path('archive/<int:year>/', PostYAV.as_view(),name='post_year_archive'),
+    path('archive/<int:year>/', PostYAV.as_view(), name='post_year_archive'),
     # Example: /blog/archive/2019/nov/
-    path('archive/<int:year>/<str:month>/', PostMAV.as_view(),name='post_month_archive'),
+    path('archive/<int:year>/<str:month>/',
+         PostMAV.as_view(), name='post_month_archive'),
     # Example: /blog/archive/2019/nov/10/
-    path('archive/<int:year>/<str:month>/<int:day>/', PostDAV.as_view(),name='post_day_archive'),
+    path('archive/<int:year>/<str:month>/<int:day>/',
+         PostDAV.as_view(), name='post_day_archive'),
     # Example: /blog/archive/today/
     path('archive/today/', PostTAV.as_view(), name='post_today_archive'),
+    # Example: /blog/tag/
+    path('tag/', TagCloudTV.as_view(), name='tag_cloud'),
+    # Example: /blog/tag/tagname/
+    path('tag/<str:tag>/', TaggedObjectLV.as_view(), name='tagged_object_list'),
 ]
